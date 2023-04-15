d116f2b618f51620ae0d90342768a5f636f4f269
yw-fang <fyuewen@gmail.com>
2023-04-15 21:49:57 +0200

    add run for windows

---
+++ run_server-windows.sh
@@ -0,0 +1,4 @@
+bundle exec jekyll --watch
+# in addition, you should open another terminal, to run
+# ruby -run -e httpd . -p 8080
+# In this case, you can view the website in your local computer with Windows 10.
