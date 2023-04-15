4dc9a21b077348fa7e569d38d94673e927395a10
yw-fang <fyuewen@gmail.com>
2023-04-15 21:51:40 +0200

    add run server instructions

--- run_server-windows.sh
+++ run_server-windows.sh
@@ -1,4 +1,4 @@
 bundle exec jekyll --watch
-# in addition, you should open another terminal, to run
+# in addition, you should open another terminal from the directory of _site, to run
 # ruby -run -e httpd . -p 8080
 # In this case, you can view the website in your local computer with Windows 10.
